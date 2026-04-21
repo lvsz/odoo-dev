f5b6fc0d045354359b81f37825bf4a60ebfa2871
Levi Siuzdak (sile) <sile@odoo.com>
2026-04-21 15:02:47 +0200

    addons/shell_plus: improve _get method
    - Allow returning more than one result with `limit` kwarg
    - Added `op` & `fname` as kwargs to pass to search
    - Also search on any `selection` field options
      - e.g. `SaleOrder._get('draft')` will return the first draft SO found
    
    Extra:
    - Added `ipdb` & `set_trace` as vars
    - Add `_env` variable allowing to auto-complete all Odoo variables
        - builtin Odoo vars are (also) stored in `_env._default`
        - models are stored in `_env.models`

--- addons/shell_plus/cli/shell.py
+++ addons/shell_plus/cli/shell.py
@@ -1,3 +1,5 @@
+from contextlib import suppress
+
 try:
     from odoo.orm.decorators import attrsetter
 except ImportError:
@@ -6,10 +8,26 @@ from odoo import api, fields
 from odoo.cli.shell import Shell
 
 
+class LocalEnv:
+    def __init__(self, default=None):
+        if default is None:
+            return
+        self.models = LocalEnv()
+        self._default = LocalEnv()
+        for key, value in default.items():
+            setattr(self._default, key, value)
+
+
 @api.model
-def _get(self, name):
-    if res := self.name_search(name, limit=1):
-        return self.browse(res[0][0])
+def _get(self, name, op='ilike', fname=None, limit=1):
+    if fname in (None, self._rec_name):
+        if res := self.name_search(name, operator=op, limit=limit):
+            return self.browse(x[0] for x in res)
+    if fname in self._fields or (fname := next((
+        field for field, values in self.fields_get([], ['selection']).items()
+        if any(name == sel[0] for sel in values.get('selection', []))
+    ), None)):
+        return self.search([(fname, op, name)], limit=limit)
     return self
 
 
@@ -19,6 +37,7 @@ def model2var(name):
 
 
 def extend_vars(env, local_vars):
+    local_env = LocalEnv(default=local_vars)
     local_vars.update(
         commit=env.cr.commit,
         rollback=env.cr.rollback,
@@ -34,20 +53,27 @@ def extend_vars(env, local_vars):
     )
     if hasattr(fields, 'Domain'):
         local_vars['Domain'] = fields.Domain
+    with suppress(ModuleNotFoundError):
+        ipdb = __import__('ipdb')
+        local_vars.update(ipbd=ipdb, set_trace=ipdb.set_trace)
+    for key, value in local_vars.items():
+        setattr(local_env, key, value)
     for ir_model in env['ir.model'].search_fetch([], ['model']):
-        local_vars[model2var(ir_model.model)] = model = env[ir_model.model]
+        model_name = model2var(ir_model.model)
+        local_vars[model_name] = model = env[ir_model.model]
+        setattr(local_env.models, model_name, model)
         if not hasattr(model.__class__, '_get'):
             setattr(model.__class__, '_get', _get)
+    local_vars['_env'] = local_env
 
 
 @attrsetter('_orig', Shell.console)
 def _patched_console(self, local_vars):
     if env := local_vars.get('env'):
         extend_vars(env, local_vars)
-    try:
+    with suppress(Exception):
         return self.ipython(local_vars)
-    except:  # noqa: E722
-        return _patched_console._orig(self, local_vars)
+    return _patched_console._orig(self, local_vars)
 
 
 Shell.console = _patched_console
